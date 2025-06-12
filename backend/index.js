e8104b27330df1a36d3daab52f8527f3d554652e
Nathan Cloutier <nathanc4571@gmail.com>
2025-06-12 12:57:41 -0400

    Add setup for SqliteDB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import setupDatabase from './utils/sqliteDbSetup.js';
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,7 @@ app.use(express.json());
 try {
     app.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`);
+        setupDatabase();
     })
 } catch (error) {
     console.error('Error starting server:', error);
---
+++ backend/utils/sqliteDbSetup.js
@@ -0,0 +1,34 @@
+import sqlite3 from 'sqlite3';
+
+export default function setupDatabase() {
+    const db = new sqlite3.Database('data/database.db', (err) => {
+        if (err) {
+            console.error('Error opening database:', err.message);
+        } else {
+            console.log('Connected to the recipes database.');
+        }
+    });
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`)
+
+
+    // ingredients should be stored in the format [{name: 'ingredient1', quantity: 'number', unit: 'unit'}, ...]
+    // instructions should be stored in the format [step1, step2, ...]
+    db.run(`
+        CREATE TABLE IF NOT EXISTS recipes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT,
+            ingredients TEXT NOT NULL,
+            instructions TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            user_id INTEGER,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        )`)
+}
